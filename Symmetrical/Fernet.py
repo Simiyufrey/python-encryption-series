6f85a14f45b1096f145d66f994738cef84e34733
Simiyufrey <simiyufrey@gmail.com>
2025-07-28 13:01:44 +0300

    updated encryption and decryption functionality, updated the code to change file extension on encryption and also change it back

--- Symmetrical/Fernet.py
+++ Symmetrical/Fernet.py
@@ -15,41 +15,45 @@ def load_key():
 
         key = f.read()
     return key
-
-
 def encrypt_message(key, message=None, filename=None):
     if key is None:
         print("Key can't be empty")
         return
-
     fernet = Fernet(key)
     if message is not None:
         fernet = Fernet(key)
         ciphertext= fernet.encrypt(message)
         print(f"encrypted message: {ciphertext}")
     elif filename is not None:
-
         if not os.path.exists(filename):
             print(os.error("File not found"))
             with open(filename, "wb") as f:
                 f.write(b"")
                 f.close()
         with open(filename, "rb") as f:
+            
             plaintext= f.read()
             ciphertext = fernet.encrypt(plaintext)
             f.close()
-        with open(filename, "wb") as f:
+        
+        
+        new_name, ext = os.path.splitext(filename)
+        print("Extension")
+        with open(f"{new_name}.enc", "wb") as f:
+            f.write(f"=={ext}==".encode('utf-8'))
             f.write(ciphertext)
             f.close()
         print(f"File Decrypted successfully {os.path.abspath(filename)}")
-    
 
+        os.remove(filename)
+
+        print("Original file deleted successfully")
+    
 def decrypt_message(key, message=None, filename=None):
     
     if key is None:
         print("Key can't be empty")
         return
-
     fernet = Fernet(key)
     if message is not None:
         fernet = Fernet(key)
@@ -61,15 +65,25 @@ def decrypt_message(key, message=None, filename=None):
             print(os.error("File not found"))
             return
         with open(filename, "rb") as f:
-            ciphertext= f.read()
-            decrpted_content = fernet.decrypt(ciphertext)
+            ciphertext= f.read().decode('utf-8')
+
+            parts = ciphertext.split('==', 2)
+            ext= parts[1]
+            content = parts[2].encode('utf-8')
+            print(content)
+            decrpted_content = fernet.decrypt(content)
             f.close()
-        with open(filename, "wb") as f:
+        new_name= os.path.splitext(filename)[0]
+        with open(f'{new_name}{ext}', "wb") as f:
             f.write(decrpted_content)
             f.close()
+           
         print(f"File Encrypted successfully {os.path.abspath(filename)}")
-    
 
+        os.remove(filename)
+
+        print("Original file deleted successfully")
+    
 def main():
 
     print('''
@@ -112,7 +126,5 @@ Welcome to GMD Encyption System
             decrypt_message(load_key(), filename= filename)
         else:
             print("Invalid choice")
-
-
 
 main()
