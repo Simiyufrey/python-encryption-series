5feb7dd081ab2a1728136c75f06c1dc9e8e7dc71
Simiyufrey <simiyufrey@gmail.com>
2025-07-29 09:52:28 +0300

    updated AES.py file

--- Symmetrical/Aes.py
+++ Symmetrical/Aes.py
@@ -2,16 +2,24 @@ from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 from Crypto.Util.Padding import pad, unpad
 
-key = get_random_bytes(16)  # AES-128
-data = b"Secret Message"
+
+key = get_random_bytes(16)
+
+data = input("Enter message to encrypt: ").encode("utf-8")
+
 cipher = AES.new(key, AES.MODE_CBC)
-ct_bytes = cipher.encrypt(pad(data, AES.block_size))
+ciphertext = cipher.encrypt(pad(data, AES.block_size))
 
-# Store IV + ciphertext
 iv = cipher.iv
-print("Encrypted:", ct_bytes)
 
-# Decrypt
+print(f"Encrypted message: {ciphertext}")
+
+
+# Decrypt message
+
 cipher = AES.new(key, AES.MODE_CBC, iv)
-pt = unpad(cipher.decrypt(ct_bytes), AES.block_size).decode('utf-8')
-print("Decrypted:", pt)
+
+plain_text = unpad(cipher.decrypt(ciphertext), AES.block_size).decode("utf-8")
+
+print(f"Decrypted message: {plain_text}")
+
