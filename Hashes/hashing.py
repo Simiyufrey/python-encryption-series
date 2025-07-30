793d1c9051c78d085c1e1120cd68933f58cc22e7
Simiyufrey <simiyufrey@gmail.com>
2025-07-30 23:07:50 +0300

    updated files, modified hashing.py for bcrypt functionality

--- Hashes/hashing.py
+++ Hashes/hashing.py
@@ -1,14 +1,27 @@
 
-
 #  Basic Hashing Example in Python
-
 import hashlib
-
+import bcrypt
 message = "hello world".encode()
 
 # SHA-256 hash
-
 sha_hash = hashlib.sha256(message)
 
 
 print("SHA-256:", sha_hash.hexdigest())
+
+
+
+#  Password Hashing (with Salt) – bcrypt
+
+password = b"Superscript"
+
+hashed_password = bcrypt.hashpw(password, bcrypt.gensalt())
+
+
+print(f"Hashed password: {hashed_password}")
+
+
+# Verifying password
+check = bcrypt.checkpw(password, hashed_password)
+print("Password match?", check)
